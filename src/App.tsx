33ce0ce4bc4dfa405f7cb8c18f6a2d97690b8e4f
DidykclickVitaliy <iiceituice@gmail.com>
2022-09-27 23:31:21 +0300

    quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,62 @@
-import React from "react";
-import { Modal } from "./components/Modal";
 import "./index.scss";
 
-function App() {
-  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
 
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: "50%" }} className="progress__inner"></div>
+      </div>
+      <h1>Что такое useState?</h1>
+      <ul>
+        <li>Это функция для хранения данных компонента</li>
+        <li>Это глобальный стейт</li>
+        <li>Это когда на ты никому не нужен</li>
+      </ul>
+    </>
+  );
+}
+
+function App() {
   return (
     <div className="App">
-      <button className="open-modal-btn" onClick={() => setIsOpen(true)}>
-        ✨ Открыть окно
-      </button>
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      <Game />
+      {/* <Result /> */}
     </div>
   );
 }
